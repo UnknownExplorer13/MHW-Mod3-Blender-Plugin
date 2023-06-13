e8fd7ad0187476b9456f80d6df1846f2ce6bec63
Ryan Cooper <45916709+Bumperdo09@users.noreply.github.com>
2023-06-12 23:19:13 -0400

    update for blender 3.5

--- blender/BlenderMod3Exporter.py
+++ blender/BlenderMod3Exporter.py
@@ -498,7 +498,7 @@ class BlenderExporterAPI(ModellingAPI):
             vTangent = list(map(round, loop.tangent*127)) + [int(loop.bitangent_sign)*127]
             if loop.vertex_index in normals and \
                 any([not (-1<=(c0-c1)<=1) for c0,c1 in zip(normals[loop.vertex_index],vNormal) ]):
-                bpy.context.scene.cursor_location = mesh.vertices[loop.vertex_index].co
+                bpy.context.scene.cursor.location = mesh.vertices[loop.vertex_index].co
                 errorHandler.duplicateNormal(loop.vertex_index, vNormal, vTangent, normals)
             else:
                 normals[loop.vertex_index] = vNormal
